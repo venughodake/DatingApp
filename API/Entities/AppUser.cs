3611da2e917aef5d467e4de4f5aaeb23c0fdda07
venughodake <venughodake22@gmail.com>
2022-02-14 13:00:33 +0530

    End of section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -24,9 +24,8 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo>Photos { get; set; }
-        // public int GetAge()
-        // {
-        //     return DateofBirth.CalculateAge();
-        // }
+     
+     public ICollection<UserLike> LikedByUsers { get; set; }
+     public ICollection<UserLike> LikedUsers { get; set; }
     }
 }
